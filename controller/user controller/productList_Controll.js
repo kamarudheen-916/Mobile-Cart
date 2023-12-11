98dc1dc7b0d27c915386f2f44d4809f7cfb49027
kamarudheen <kamar916@gmail.com>
2023-12-11 11:50:51 +0530

    ready for second   hosting4

--- controller/user controller/productList_Controll.js
+++ controller/user controller/productList_Controll.js
@@ -12,42 +12,12 @@ const get_product_List = async(req,res)=>{
         try {
             
         req.session.product_category = req.params.category
-        const product_category= req.session.product_category
-        const category =product_category
-        // console.log('----------productId:',product_category);
+        const category =req.session.product_category
         const username= req.session.user
-        let products_count=  await allProducts.find({category:product_category}).count()
-        let pageNumber = req.query.page?Number(req.query.page):0; 
-        products_count = products_count/5
-        let skip =pageNumber*5
-        // console.log('---skip',skip);
-        let products =await allProducts.find({category:product_category}).skip(skip).limit(5)
-        // Fetch the user's cart data
-        const user = await userCollection.findOne({ username });
-        // console.log('------------------------userCartData', user);
-
-        // Fetch the user's cart data
-        const userCartData = await cartCollection.findOne({ userId: user._id });
-        // console.log('------------------------userCartData', userCartData);
-
-        // Declare cartProductIds outside the if block
-        let cartProductIds = new Set();
-
-        // Check if userCartData is not null and has 'Products'
-        if (userCartData && userCartData.Products) {
-            // Create a Set of productIds that are in the cart
-            cartProductIds = new Set(userCartData.Products.map(item => item.ProductId.toString()));
-            // console.log('------------------------cartProductIds', cartProductIds);
-        }
-
-        const wishlist_ = await WishlistCollection.findOne({ User_Id: user._id });
-        const wishlist = wishlist_ ? wishlist_.Products : [];
-        
         const allcategorys = await categoryCollection.find({},{name:1,_id:0})
         // console.log('categoryCollection--------------',allcategorys);
 
-        res.render('user/userProductList',
-        {title:'Product List Page',username,products,products_count,category,pageNumber,wishlist,cartProductIds,allcategorys})
+        res.render('user/userProductList',{title:'Product List Page',username,category,allcategorys})
        
         } catch (error) {
             console.log('----------product list  error:----',error);
@@ -73,7 +43,7 @@ const filterByCategory = async(req,res)=>{
          if (userCartData && userCartData.Products) {
              // Create a Set of productIds that are in the cart
              cartProductIds = new Set(userCartData.Products.map(item => item.ProductId.toString()));
-            //  console.log('------------------------cartProductIds', cartProductIds);
+
          }
          
         //  console.log('------------------------cartProductIds', cartProductIds);
